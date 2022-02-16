022c75e69c9702fbe617059f1072a4b9f97df606
unclechuen <33083306+unclechuen@users.noreply.github.com>
2022-02-17 01:12:30 +0800

    init

---
+++ src/main/java/com/example/toursystem/TourSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.toursystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourSystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/toursystem/controller/home.java
@@ -0,0 +1,14 @@
+package com.example.toursystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class home {
+
+    @GetMapping("/")
+    public String homepage(Model model){
+        return "home";
+    }
+}
