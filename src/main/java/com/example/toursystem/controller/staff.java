468d346c2738b56e7654c497ab3bcce9da1944f0
unclechuen <33083306+unclechuen@users.noreply.github.com>
2022-02-17 18:01:15 +0800

    minor chnage

---
+++ src/main/java/com/example/toursystem/controller/staff.java
@@ -0,0 +1,10 @@
+package com.example.toursystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/staff")
+public class staff {
+
+}
---
+++ src/main/java/com/example/toursystem/controller/tourist.java
@@ -0,0 +1,9 @@
+package com.example.toursystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tourist")
+public class tourist {
+}
