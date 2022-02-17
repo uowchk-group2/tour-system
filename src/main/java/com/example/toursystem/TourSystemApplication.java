33f6aa1e2d8d6d598dd52c47b532d7579a188270
unclechuen <33083306+unclechuen@users.noreply.github.com>
2022-02-17 13:55:55 +0800

    minor change

--- src/main/java/com/example/toursystem/TourSystemApplication.java
+++ src/main/java/com/example/toursystem/TourSystemApplication.java
@@ -2,9 +2,10 @@ package com.example.toursystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TourSystemApplication {
+public class TourSystemApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TourSystemApplication.class, args);
