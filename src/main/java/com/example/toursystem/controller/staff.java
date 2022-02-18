14c2c542df4f149fa955fe08cbaa9b6035aaf839
unclechuen <33083306+unclechuen@users.noreply.github.com>
2022-02-18 13:47:02 +0800

    header change

--- src/main/java/com/example/toursystem/controller/staff.java
+++ src/main/java/com/example/toursystem/controller/staff.java
@@ -1,10 +1,17 @@
 package com.example.toursystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/staff")
 public class staff {
+
+    @GetMapping("")
+    public String demopage(Model model){
+        return "staff/staffWelcome";
+    }
 
 }
