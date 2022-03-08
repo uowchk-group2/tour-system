86f695720870334c9448bf6b8ba43ad352e2fefc
unclechuen <33083306+unclechuen@users.noreply.github.com>
2022-03-09 00:28:50 +0800

    Staff: User update

--- src/main/java/com/example/toursystem/controller/StaffController.java
+++ src/main/java/com/example/toursystem/controller/StaffController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/staff")
 public class StaffController {
@@ -19,7 +21,10 @@ public class StaffController {
     }
 
     @GetMapping("user")
-    public String user(Model model){
+    public String user(Model model, HttpServletRequest request){
+        String username = request.getUserPrincipal().getName();
+        model.addAttribute("username",username);
+
         return "staff/user";
     }
 
